212a264e8ca72c9bc509eed40c0cafec55c59d5c
mongolyy <mongolyy@gmail.com>
2022-01-14 00:30:36 +0900

    deriveマクロを使った記述に変更

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
-use clap::{App, Arg};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Your name",
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
 
 fn main() {
-    let matches = App::new("My RPN program")
-        .version("1.0.0")
-        .author("Your name")
-        .about("Super awesome sample RPN calculator")
-        .arg(Arg::new("formula_file")
-                 .value_name("FILE")
-                 .index(1)
-                 .required(false),
-        )
-        .arg(Arg::new("verbose")
-            .short('v')
-            .long("verbose")
-            .required(false),
-        )
-        .get_matches();
+    let opts = Opts::parse();
 
-    match matches.value_of("formula_file") {
+    match opts.formula_file {
         Some(file) => println!("File specified: {}", file),
         None => println!("No file specified."),
     }
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified?: {}", verbose);
+    println!("Is verbosity specified?: {}", opts.verbose);
 }
