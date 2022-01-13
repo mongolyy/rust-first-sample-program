16e04bc1d67a845cdc74168484a9944864355707
mongolyy <mongolyy@gmail.com>
2022-01-14 00:17:48 +0900

    clapクレートの導入

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("My RPN program")
+        .version("1.0.0")
+        .author("Your name")
+        .about("Super awesome sample RPN calculator")
+        .arg(Arg::new("formula_file")
+                 .value_name("FILE")
+                 .index(1)
+                 .required(false),
+        )
+        .arg(Arg::new("verbose")
+            .short('v')
+            .long("verbose")
+            .required(false),
+        )
+        .get_matches();
+
+    match matches.value_of("formula_file") {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+    let verbose = matches.is_present("verbose");
+    println!("Is verbosity specified?: {}", verbose);
 }
